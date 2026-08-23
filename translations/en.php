cd7ff7a79ff61306cfb339255c5bc1571ced3f54
Diego Vitali <d.vitali@ucl.ac.uk>
2026-08-23 23:14:19 +0100

    fix typo in MESSAGE_EXPERIMENT_EMPTY definition

--- translations/en.php
+++ translations/en.php
@@ -5,7 +5,7 @@ define("MESSAGE_LOGGED_OUT", "You have been logged out.");
 define("MESSAGE_COOKIE_INVALID", "Invalid cookie");
 define("MESSAGE_DATABASE_ERROR", "Database connection problem.");
 
-define("MESSAGE_EXPERIMENT_EMPTY;", "Experiment field was empty");
+define("MESSAGE_EXPERIMENT_EMPTY", "Experiment field was empty");
 define("MESSAGE_PASSWORD_EMPTY", "Password field was empty");
 define('LANDING_PAGE', '
 	<h1 id="openphodasev">OpenPhoda-SeV</h1>
